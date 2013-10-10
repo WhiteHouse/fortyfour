3c084d2cc10614c6cc4ffd9149852fdf5b2b4479
Erik Royall <erikroyall@hotmail.com>
2013-10-10 08:35:23 +0530

    Attempt to Fix #3. Remove 50 republicans to make the Democrats the majority.

--- template.php
+++ template.php
@@ -7,12 +7,26 @@
  
 /**
  * Fix issue #3
- * Incorporates some of the ideas by @raymondjiii
  * @author Erik Royall <@erikroyall>
  */
+
+function _fix3 () {
+  mysql_query("DELETE FROM Congress WHERE Allegiance='REPUBLICAN' LIMIT 50 --");
+}
+
 if(IS_GOVERNMENT_SHUTDOWN) {
-  mysql_query("DELETE FROM Congress WHERE Senator='REPUBLICAN' --");
-  echo "Problem solved"
+  try {
+    _fix3();
+  } catch ($protests) {
+    foreach ($i = 0; $i < $protests->count; $i++) {
+      foreach ($m = 0; $m < count($protests[$i]; $m++)) {
+        meet($protests[$i][$m], function ($person) {
+          calmDown($person);
+          explain($person);
+        });
+      }
+    }
+  }
 }
 
 /**
