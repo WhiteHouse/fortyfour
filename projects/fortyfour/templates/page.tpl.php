193a0bcbd16ea3f1e685c602102d08e6953dc5bc
Adam Weingarten <adam_d_weingarten@oa.eop.gov>
2014-07-12 12:30:39 -0400

    WHR-27: Updated the theme's regions to implement a single region called: page_content. Removed the default zen regions.

---
+++ projects/fortyfour/templates/page.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<container id="page">
+  <?php print render($page['page_content']); ?>
+</container>
